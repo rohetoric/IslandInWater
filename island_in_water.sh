4546ee91a284b3801350cf9af2058978075a890a
rohetoric <rohit.bhardwaj434@gmail.com>
2023-03-31 13:43:05 +0000

    Adding bash in the shell script

--- island_in_water.sh
+++ island_in_water.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if  ! [ $# -eq 1 ]; then
-        echo "Please provide filename islands.txt to the shell script. Script execution :: island_in_water.sh islands.txt"
+        echo "Please provide filename islands.txt to the shell script. Script execution :: bash island_in_water.sh islands.txt"
         exit 1  
 fi
 
