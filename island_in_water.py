49499c9cbf7d34f0a31c91ddb87553a7beda627b
rohetoric <rohit.bhardwaj434@gmail.com>
2023-03-31 11:23:23 +0000

    Reading from the file

--- island_in_water.py
+++ island_in_water.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+import sys
+import os
+
+class IslandsInWater():
+  def __init__(self, file_name,island_matrix=[]):
+    self.file_name = file_name
+    self.island_matrix = []
+
+  def read_from_file(self):
+    with open(self.file_name, 'r') as f:
+      data = f.readlines() # reading into matrix
+    for raw_line in data:
+      split_line = raw_line.strip().split(" ")
+      nums_ls = [int(x) for x in split_line] #string conversion to integer
+      self.island_matrix.append(nums_ls)
+
+    
+def main(args):
+  filename = args[0]
+  islandsinwater = IslandsInWater(filename)
+  islandsinwater.read_from_file()
+
+  #write the code to count number of islands now..
+
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
